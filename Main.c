dbd269bbf80cc7d851c7db66f9e4b71c1bf7adf0
David Reiss <dreiss@fb.com>
2015-08-05 15:58:48 -0700

    Use I2C to initiate key presses
    - Drop the old code that was reading input pins directly.
    - Listen to a fixed I2C address of 'k' (0x6B).
    - Add a TWI interrupt handler to properly handle the I2C bus.
      Unfortunately, there appears to be no way to only ACK writes,
      so we respond to reads as sensibly as possible.
      When a write is received, we store the first byte as a command
      (as long as one is not being processed).
    - Keypress commands are processed by a simple state machine.
      When we receive a key command, we reset our timer and set they key as
      pressed.  When the timer hits a specified point (variable per
      command), we release it, but force the keyboard to stay idle for
      another (fixed) time interval.  Finally, we clear the current command
      and allow more commands to be sent.  Commands received during a
      keypress or the release period are ignored.

--- Main.c
+++ Main.c
@@ -1,6 +1,10 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
 #include <avr/io.h>
 #include <avr/wdt.h>
 #include <avr/power.h>
+#include <util/twi.h>
 
 #include <LUFA/Drivers/Board/LEDs.h>
 #include <LUFA/Drivers/Peripheral/Serial.h>
@@ -10,27 +14,54 @@
 #define SERIAL_SPEED (115200)
 
 
+static const uint8_t TWI_ADDRESS  = 'k';
+
+// Convert ms to Timer/Counter1 ticks.  1024 factor is from prescaler.
+#define MS_TO_TICKS(ms) (ms * (F_CPU / 1000) / 1024)
+static const uint16_t KEY_RELEASE_TIME_TICKS = MS_TO_TICKS(50);
+static const uint16_t NORMAL_KEYPRESS_TIME_TICKS = MS_TO_TICKS(50);
+static const uint16_t LONG_KEYPRESS_TIME_TICKS = MS_TO_TICKS(2000);
+#undef MS_TO_TICKS
+
+enum {
+  // Idle = waiting for command over I2C.
+  STATE_IDLE = 0,
+  // Currently pressing a key.  I2C input ignored.
+  STATE_PRESSING = 1,
+  // Waiting with keys released for a short time.  I2C input ignored.
+  STATE_RELEASING = 2,
+};
+
+// We could disable I2C ACKs during STATE_PRESSING and STATE_RELEASING,
+// but I don't think it's important.
+
+// Current execution state.
+static uint8_t g_state;
+// Command received over I2C.  Only set by I2C ISR, and only cleared
+// in drive_logic.
+static uint8_t twi_cmd;
+// Key code currently being pressed.
+static uint8_t pressed_code;
+// Timestamp in Timer/Counter1 at which we should transition states.
+static uint16_t transition_time;
+
 // HID device config and state.  Defined in HidSetup.c.
 extern USB_ClassInfo_HID_Device_t kbd_device;
 
 
+// Forward declarations.
 static void setup_hardware(void);
+static void check_usb_device_state(void);
+static void drive_logic(void);
+static bool process_cmd(void);
 
 
 int main(void) {
   setup_hardware();
 
-  uint8_t last_state = 0xff;
-
   for (;;) {
-    uint8_t state = USB_DeviceState;
-    if (state != last_state) {
-      last_state = state;
-      Serial_SendByte('S');
-      Serial_SendByte(state + '0');
-      Serial_SendByte('\n');
-    }
-
+    check_usb_device_state();
+    drive_logic();
     HID_Device_USBTask(&kbd_device);
     USB_USBTask();
   }
@@ -48,8 +79,179 @@ static void setup_hardware(void) {
   // Force re-enumeration like the Trinket HidCombo demo.
   // Without the sleep, the computer still thinks the bootloader is attached.
   Delay_MS(500);
+
+  // Set up Timer1 to run at CLK/1024, or 1 tick per 128 us at 8 MHz.
+  TCCR1A = 0;
+  TCCR1B = _BV(CS12) | _BV(CS10);
+
+  // Make sure the TWI pins are set as inputs with pull-ups off.
+  // This should be the default, but there's no harm in re-setting them.
+  // Of course, if I were really afraid of damaging a Raspberry Pi,
+  // I'd probably do this in the bootloader, or at least before
+  // the 500 ms sleep.
+  DDRD &= ~_BV(PD0);
+  DDRD &= ~_BV(PD1);
+  PORTD &= ~_BV(PD0);
+  PORTD &= ~_BV(PD1);
+  // Set up TWI as a slave with ACKs and interrupts enabled,
+  // but ignoring General Calls.
+  TWAR = (TWI_ADDRESS << 1);
+  TWCR = _BV(TWEN) | _BV(TWIE) | _BV(TWINT) | _BV(TWEA);
+
   USB_Init();
   GlobalInterruptEnable();
+}
+
+static void check_usb_device_state(void) {
+  // Dump USB connection state changes to the serial connection.
+  static uint8_t last_state = 0xff;
+  uint8_t state = USB_DeviceState;
+  if (state != last_state) {
+    last_state = state;
+    Serial_SendByte('S');
+    Serial_SendByte(state + '0');
+    Serial_SendByte('\n');
+  }
+}
+
+static inline void reset_timer(void) {
+  TCNT1 = 0;
+  TIFR1 = _BV(TOV1);
+}
+
+static inline bool timer_expired(void) {
+  return (TCNT1 > transition_time) || (TIFR1 & _BV(TOV1));
+}
+
+static void drive_logic(void) {
+  switch (g_state) {
+    case STATE_IDLE:
+      if (process_cmd()) {
+        g_state = STATE_PRESSING;
+        reset_timer();
+        Serial_SendByte('a' - 1 + twi_cmd);
+      }
+      break;
+    case STATE_PRESSING:
+      if (timer_expired()) {
+        g_state = STATE_RELEASING;
+        pressed_code = 0;
+        reset_timer();
+        transition_time = KEY_RELEASE_TIME_TICKS;
+      }
+      break;
+    case STATE_RELEASING:
+      if (timer_expired()) {
+        g_state = STATE_IDLE;
+        twi_cmd = 0;
+      }
+      break;
+  }
+}
+
+// Examine twi_cmd and handle it:
+// - If 0, so command has been sent.  Return false.
+// - If a valid command, update pressed_code and transition_time,
+//   and return true.
+// - Else, clear the invalid command and return false.
+static bool process_cmd(void) {
+  switch (twi_cmd) {
+    case 0:
+      return false;
+
+#define key(cmd, len, code) \
+  case cmd: \
+    pressed_code = CONCAT_EXPANDED(HID_KEYBOARD_SC_, code); \
+    transition_time = CONCAT_EXPANDED(len, _KEYPRESS_TIME_TICKS); \
+    return true
+
+    key(0x03, NORMAL, UP_ARROW);
+    key(0x04, NORMAL, DOWN_ARROW);
+    key(0x05, NORMAL, LEFT_ARROW);
+    key(0x06, NORMAL, RIGHT_ARROW);
+    key(0x07, NORMAL, KEYPAD_ENTER);
+
+    key(0x08, NORMAL, ESCAPE);
+    key(0x09, NORMAL, F12);
+    key(0x0a, NORMAL, APPLICATION);
+    key(0x0b, NORMAL, MEDIA_PREVIOUS_TRACK);
+    key(0x0c, NORMAL, SPACE);
+    key(0x0d, NORMAL, MEDIA_NEXT_TRACK);
+
+    key(0x0e, LONG,   MEDIA_PREVIOUS_TRACK);
+    key(0x0f, LONG,   MEDIA_NEXT_TRACK);
+
+#undef key
+  }
+
+  twi_cmd = 0;
+  return false;
+}
+
+ISR(TWI_vect) {
+  switch (TW_STATUS) {
+    // We have been addressed as the slave in slave-receive (write) mode,
+    // and an ACK has been sent.  Continue the transaction.
+    // (Note, we don't expect ARB_LOST or GCALL, but whatever.)
+    case TW_SR_SLA_ACK:
+    case TW_SR_ARB_LOST_SLA_ACK:
+    case TW_SR_GCALL_ACK:
+    case TW_SR_ARB_LOST_GCALL_ACK:
+      TWCR = _BV(TWEN) | _BV(TWIE) | _BV(TWINT) | _BV(TWEA);
+      break;
+    // We have received the first byte.  Store it in twi_cmd if it is empty.
+    // Clear TWEA to ensure we NACK any further written bytes.
+    case TW_SR_DATA_ACK:
+    case TW_SR_GCALL_DATA_ACK:
+      if (twi_cmd == 0) {
+        twi_cmd = TWDR;
+      }
+      TWCR = _BV(TWEN) | _BV(TWIE) | _BV(TWINT);
+      break;
+    // The master sent us more bytes.  We sent a NACK and
+    // the transceiver will automatically switch to "not addressed".
+    // Turn TWEA back on so we can ACK the next transmission.
+    // (The transceiver won't ACK anything until the next start condition.)
+    case TW_SR_DATA_NACK:
+    case TW_SR_GCALL_DATA_NACK:
+      TWCR = _BV(TWEN) | _BV(TWIE) | _BV(TWINT) | _BV(TWEA);
+      break;
+    // We got a stop condition.  Return to our base state.
+    case TW_SR_STOP:
+      TWCR = _BV(TWEN) | _BV(TWIE) | _BV(TWINT) | _BV(TWEA);
+      break;
+
+    // We have been addressed as the slave in slave-transmit (read) mode,
+    // and an ACK has been sent (which is not really ideal).
+    // We have nothing to say, so just send a ~0 as the "last data byte".
+    // This is (kind of) consistent with a device that does not respond.
+    case TW_ST_SLA_ACK:
+    case TW_ST_ARB_LOST_SLA_ACK:
+    case TW_ST_DATA_ACK:
+      TWDR = ~0;
+      TWCR = _BV(TWEN) | _BV(TWIE) | _BV(TWINT);
+      break;
+    // We got a NACK on data (or "last data", I think) or
+    // an ACK on "last data" (master keeps reading).
+    // The transceiver will automatically switch to "not addressed".
+    // Turn TWEA back on so we can ACK the next transmission.
+    // (The transceiver won't ACK anything until the next start condition.)
+    case TW_ST_DATA_NACK:
+    case TW_ST_LAST_DATA:
+      TWCR = _BV(TWEN) | _BV(TWIE) | _BV(TWINT) | _BV(TWEA);
+      break;
+
+    // In the case of a bus error, we need to set TWSTO to reset everything.
+    case TW_BUS_ERROR:
+      TWCR = _BV(TWEN) | _BV(TWIE) | _BV(TWEA) | _BV(TWINT) | _BV(TWSTO);
+      break;
+
+    // This should not happen because we never enter master mode.
+    default:
+      Serial_SendString_P(PSTR("TWms"));
+      Serial_SendByte(TW_STATUS);
+      abort();
+  }
 }
 
 bool CALLBACK_HID_Device_CreateHIDReport(
@@ -64,32 +266,7 @@ bool CALLBACK_HID_Device_CreateHIDReport(
 
   USB_KeyboardReport_Data_t* kbd_report = (USB_KeyboardReport_Data_t*)ReportData;
 
-  PORTB |= _BV(PB4) | _BV(PB5) | _BV(PB6) | _BV(PB7);
-  PORTC |= _BV(PC6);
-  PORTD |= _BV(PD0) | _BV(PD1) | _BV(PD4) | _BV(PD6) | _BV(PD7);
-  PORTE |= _BV(PE6);
-
-#define key(port, bit, code) do { \
-    if (!(CONCAT_EXPANDED(PIN, port) & \
-          _BV(CONCAT_EXPANDED(CONCAT_EXPANDED(P, port), bit)))) { \
-      kbd_report->KeyCode[0] = CONCAT_EXPANDED(HID_KEYBOARD_SC_, code); \
-    } \
-} while (0)
-
-  key(B, 4, UP_ARROW);
-  key(B, 5, DOWN_ARROW);
-  key(B, 6, LEFT_ARROW);
-  key(B, 7, RIGHT_ARROW);
-  key(D, 6, KEYPAD_ENTER);
-
-  key(D, 1, ESCAPE);
-  key(D, 0, F12);
-  key(D, 4, APPLICATION);
-  key(C, 6, MEDIA_PREVIOUS_TRACK);
-  key(D, 7, SPACE);
-  key(E, 6, MEDIA_NEXT_TRACK);
-
-#undef key
+  kbd_report->KeyCode[0] = pressed_code;
 
   *ReportSize = sizeof(USB_KeyboardReport_Data_t);
   return false;
